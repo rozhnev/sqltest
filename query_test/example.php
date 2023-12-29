5ecbd229fc25bbf76965ea63ef32e8aad5ca36e1
Slava Rozhnev <rozhnev@msn.com>
2023-12-29 18:05:40 +0200

    template updated

--- query_test/example.php
+++ query_test/example.php
@@ -1,3 +1,7 @@
 <?php
-$queryRegexValidator = ''; // Regexp expression to validate query string. If empty - no query validation validation
+// Regexp expression to validate query string. If array empty - no query validation validation
+$queryRegexValidator = [
+    //  'queryMatch' => '//',
+    //  'queryNotMatch' => '//',
+];
 $validJsonResult = '[Query result in JSON format]'; 
