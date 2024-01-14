f8fa409ab4a6e388c8c3c5af7ccbc3e183cb62d2
SlavaRozhnev <slava.rozhnev@pandats.com>
2024-01-14 16:38:49 +0200

    AcceptanceTester

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,35 @@
+<?php
+
+class FirstCest
+{
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('place where you can test your SQL knowledge for free');
+    }
+
+    public function frontpageEnWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/en');
+        $I->see('place where you can test your SQL knowledge for free');
+    }
+
+    public function frontpageRuWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/ru');
+        $I->see('место, где вы можете бесплатно проверить свои знания SQL');
+    }
+
+    public function privacyPolicyEnWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/en/privacy-policy');
+        $I->see('Privacy Policy for SQLtest');
+    }
+
+    public function privacyPolicyRuWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/ru/privacy-policy');
+        $I->see('Политика конфиденциальности для SQLtest');
+    }
+
+}
