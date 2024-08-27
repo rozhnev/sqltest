58625841c18de5bbe0125443ad08bd82c76e2e52
rozhnev <slava.rozhnev@pandats.com>
2024-08-27 17:22:42 +0300

    referral_link active_till added

--- lib/Helper.php
+++ lib/Helper.php
@@ -3,7 +3,12 @@ class Helper
 {
     public static function getReferralLink(PDO $dbh, string $lang): string
     {
-        $stmt = $dbh->prepare("SELECT referral_link AS referralLink FROM referral_links WHERE lang = :lang AND not deleted ORDER BY random() LIMIT 1;");
+        $stmt = $dbh->prepare(
+            "SELECT referral_link AS referralLink 
+            FROM referral_links 
+            WHERE lang = :lang AND NOT deleted AND (active_till IS NULL OR active_till > CURRENT_DATE)
+            ORDER BY random() LIMIT 1;"
+        );
         $stmt->execute([':lang' => $lang]);
         return $stmt->fetchColumn(0);
     }
