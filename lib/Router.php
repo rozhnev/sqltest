3aed7650820f66fb1738791f78cd836e548c2016
Slava Rozhnev <rozhnev@msn.com>
2025-04-11 13:36:43 +0300

    old route

--- lib/Router.php
+++ lib/Router.php
@@ -40,11 +40,18 @@ class Router
 
             }
         }
+        if (preg_match("@(?<lang>ru|en|pt)/(?<action>question)/(?<questionCategory>\d+)/(?<question>\d+)@i", $path, $params)) {
+            $this->controller->setLanguge($params['lang'] ?? DEFAULT_LANGUAGE);
+        } else {
+            $this->controller->setLanguge(DEFAULT_LANGUAGE);
+        }
+        
         if (preg_match("@(?<lang>ru|en|pt)/@i", $path, $params)) {
             $this->controller->setLanguge($params['lang'] ?? DEFAULT_LANGUAGE);
         } else {
             $this->controller->setLanguge(DEFAULT_LANGUAGE);
         }
+
         return $this->controller->question([
             'questionID'            => 1,
             'questionCategory'      => $env['DEFAULT_QUESTIONS_CATEGORY'] ?? 'sql-basics',
