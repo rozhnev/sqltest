379d71f2937868c9d031bf587b8bbcd3e8fdd56b
rozhnev <slava.rozhnev@pandats.com>
2026-08-12 17:46:02 +0300

    Strip paragraph from translation

--- lib/LLM.php
+++ lib/LLM.php
@@ -49,6 +49,8 @@ class LLM {
         $parsed = preg_replace('#</?(ul|ol)>#i', '', $parsed);
         $parsed = preg_replace('#<li>#i', '- ', $parsed);
         $parsed = preg_replace('#</li>#i', "\n", $parsed);
+        $parsed = preg_replace('#<p>#i', '- ', $parsed);
+        $parsed = preg_replace('#</p>#i', "\n", $parsed);
         foreach (["<span class='sql'>", '</span>', '<b>', '</b>'] as $tag) {
             $parsed = str_replace($tag, htmlspecialchars($tag, ENT_QUOTES | ENT_HTML5), $parsed);
         }
